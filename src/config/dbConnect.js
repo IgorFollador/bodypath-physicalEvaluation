111609d258523369635b5462e7d52b0f1d6529c4
Igor Follador <igorledf@hotmail.com>
2022-06-15 13:08:00 -0300

    Alter Database

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/bodypath-dev?");
+mongoose.connect("mongodb+srv://root:root@bodypath.ojocqyp.mongodb.net/?retryWrites=true&w=majority");
 
 let db = mongoose.connection;
 
