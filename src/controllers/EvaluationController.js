00cd105962bb6a41639366a1be8483c4903e2f77
Igor Follador <igorledf@hotmail.com>
2022-06-18 03:44:16 -0300

    Create methods to read, create, update and delete Evaluations

--- src/controllers/EvaluationController.js
+++ src/controllers/EvaluationController.js
@@ -10,7 +10,53 @@ class EvaluationController {
                         model: database.Users,
                         attributes: ['firstName', 'lastName']
                     },
-                    attributes: {exclude: ['createdAt', 'updatedAt']}
+                    attributes: {exclude: ['professional_id', 'createdAt', 'updatedAt']}
+                  }
+            });
+            return res.status(200).json(allEvaluations);
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+
+    static async readEvaluation(req, res) {
+        const { id } = req.params;
+
+        try {
+            const evaluation = await database.Evaluations.findOne({
+                include: {
+                    model: database.Clients,
+                    include: {
+                        model: database.Users,
+                        attributes: ['firstName', 'lastName']
+                    },
+                    attributes: {exclude: ['professional_id', 'createdAt', 'updatedAt']}
+                  },
+                  where: {
+                    id: id
+                  }
+            });
+            return res.status(200).json(evaluation);
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+
+    static async readAllEvaluationsByProfessionalId(req, res) {
+        const { id } = req.params;
+        
+        try {
+            const allEvaluations = await database.Evaluations.findAll({
+                include: {
+                    model: database.Clients,
+                    include: {
+                        model: database.Users,
+                        attributes: ['firstName', 'lastName']
+                    },
+                    attributes: {exclude: ['professional_id', 'createdAt', 'updatedAt']}
+                  },
+                  where: {
+                    professional_id: id
                   }
             });
             return res.status(200).json(allEvaluations);
@@ -20,20 +66,79 @@ class EvaluationController {
         }
     }
 
-    static async readAllEvaluationsByProfessionalUserId(req, res) {
-
+    static async readAllEvaluationsNamesByProfessionalId(req, res) {
+        const { id } = req.params;
+        try {
+            const allEvaluations = await database.Evaluations.findAll({
+                include: {
+                    model: database.Clients,
+                    include: {
+                        model: database.Users,
+                        attributes: ['firstName', 'lastName']
+                    },
+                    attributes: {exclude: ['professional_id', 'createdAt', 'updatedAt']}
+                  },
+                  attributes: ['id', 'createdAt', 'updatedAt'],
+                  where: {
+                    professional_id: id
+                  }
+            });
+            return res.status(200).json(allEvaluations);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: error.message });
+        }
     }
 
-    static async readAllEvaluationsNamesByProfessionalUserId(req, res) {
+    static async createEvaluation(req, res) {
+        const formEvaluation = req.body;
+        const client = await database.Clients.findByPk(formEvaluation.client_id);
+        if(client === null) return res.status(404).json({ message: 'Client not found'})
+        const professional = await database.Professionals.findByPk(formEvaluation.professional_id);
+        if(professional === null) return res.status(404).json({ message: 'Professional not found'})
 
+        try {
+            const evaluation = await database.Evaluations.create(formEvaluation);
+            return res.status(201).json(evaluation);
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
     }
 
-    static createEvaluation(req, res) {
-
+    static async updateEvaluation(req, res) {
+        const { id } = req.params;
+        const formEvaluation = req.body;
+        try {
+            const evaluation = await database.Evaluations.findByPk(id);
+            if(evaluation === null) return res.status(404).json({ message: 'Evaluation not found' });
+            await database.Evaluations.update(formEvaluation, {
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json({ message: `ID ${id} updated` });
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
     }
 
-    static updateEvaluation(req, res) {
+    static async deleteEvaluation(req, res) {
+        const { id } = req.params;
         
+        try {
+            const evaluation = await database.Evaluations.findByPk(id);
+            if(evaluation === null) return res.status(404).json({ message: 'Evaluation not found'})
+
+            await database.Evaluations.destroy({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json({ message: `Evaluation with ID ${id} deleted` });
+
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
     }
 }
 
